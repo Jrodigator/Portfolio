2024cd2e7bedce30d71b3fbc459de32c59c531c3
Rodrigues, J, Mr [25983792@sun.ac.za] <25983792@sun.ac.za>
2024-01-21 19:24:46 +0200

    ham menu, mobile #2

--- src/app/dashboard/navbar.tsx
+++ src/app/dashboard/navbar.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./navbar.module.scss";
 
 const Navbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+  const handleHamClick = () => {
+    console.log("clicked");
+    setIsNavExpanded(!isNavExpanded);
+  };
+  // () => setIsNavExpanded(!isNavExpanded)
   return (
     <nav className={styles.transparentnavbar}>
       <h1 className={styles.logo}>Jared Rodrigues</h1>
-      <div className={styles.navitems}>
+      
+      {/* Hamburger Menu */}
+      <div className={`${styles.hamburger}`} onClick={handleHamClick}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
+      {/* Nev Items */}
+      <div className={`${styles.navitems} ${isNavExpanded ? styles.show : ""}`}>
         <a className={styles.navitem} href="#home">Home</a>
         <a className={styles.navitem} href="#about">About</a>
         <a className={styles.navitem} href="#services">Gallery</a>
